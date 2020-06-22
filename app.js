ac4f793a168f614d3591705e87a95759a633a491
avyrie <pittman.ally@gmail.com>
2020-06-22 16:33:48 -0600

    random factor will be increased every 5 seconds

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ class DigitalPet {
 }
 
 //Randomizes starting hunger, sleepiness, and boredom values
-let randomHunger = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-let randomSleepiness = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-let randomBoredom = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+let randomHunger = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+let randomSleepiness = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+let randomBoredom = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
 let name;
 
 //Creates tamagotchi
@@ -75,17 +75,63 @@ function assignBoredom () {
 
 //-----------------------------------------------------------------
 
-//Starts timer. Game lasts for 30 minutes (1800 seconds)
+//Puts tamagotchi stats into an array for easy access
+
+// console.log(factors);
+
+//Starts timer. Game lasts for 20 minutes (1200 seconds)
 let time = 0;
 function startTimer() {
 
             const timer = setInterval(function () {    
-            if (time < 1800) {
+            if (time < 1200) {
                 time++;
                 console.log(time);
             } else {
                 console.log('It\'s always sad to say goodbye');
                 clearInterval(timer);
             }
+
+            let factors = [tamagotchi.hunger, tamagotchi.sleepiness, tamagotchi.boredom ];
+            //every 5 seconds,a prompt will appear and a ranomly chosen factor will be increased by 1
+            if (time % 5 === 0) {
+
+                let that = factors[Math.floor(Math.random() * 3)];
+
+                if (that === tamagotchi.hunger) {
+
+                        tamagotchi.hunger = tamagotchi.hunger + 1;
+                        console.log(`${tamagotchi.name}'s hunger level is ${tamagotchi.hunger}`);
+                        hungerCount.textContent = tamagotchi.hunger;
+                }
+
+                // } else if (that === tamagotchi.sleepiness) {
+                //     ///grab sleepy
+
+                // } else if (that === tamagotchi.boredom) {
+                //     //grab boredom
+                // }
+                // that++
+                // console.log(that);
+                
+                //come back to this. need to fins a way to reflect increased stat in html***
+                
+                window.alert(`${tamagotchi.name} needs some attention`);
+            }   
+
+             //increases age every 60 seconds
+             if (time % 60 === 0) {
+                tamagotchi.age++
+                console.log(`Happy birthday, ${tamagotchi.name}! You're ${tamagotchi.age} years old!`)
+            }
+
         }, 1000) // interval
+    }
+
+    //---------------------------------------------------------
+
+    
+    //if one of the metrics goes above 10, pet will die
+    if (tamagotchi.hunger >= 10 || tamagotchi.boredom >= 10 || tamagotchi.sleepiness >= 10) {
+        console.log(`You have let down your precious friend. They have died from neglect.`)
     }
